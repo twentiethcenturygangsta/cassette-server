c12553fb3fb314945a2bbdca152ab166f8488567
oereo <dlstpgns0406@gmail.com>
2022-12-09 20:17:29 +0900

    chore: initialize project

---
+++ src/main/java/com/playlist/cassette/CassetteApplication.java
@@ -0,0 +1,13 @@
+package com.playlist.cassette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassetteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassetteApplication.class, args);
+	}
+
+}
