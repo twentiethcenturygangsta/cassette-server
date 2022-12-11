2b586b99b013ffd543ef4d7a7d35565428b5099f
oereo <dlstpgns0406@gmail.com>
2022-12-11 19:06:18 +0900

    feat: BaseAuditEntity class 생성 - createdAt - lastModifiedAt : 마지막 수정 시점이기 때문에 last 추가 - isRemoved

--- src/main/java/com/playlist/cassette/entity/BaseAuditEntity.java
+++ src/main/java/com/playlist/cassette/entity/BaseAuditEntity.java
@@ -1,4 +1,26 @@
 package com.playlist.cassette.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
 public class BaseAuditEntity {
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "last_modified_at", nullable = false)
+    private LocalDateTime lastModifiedAt;
+
+    @Column(columnDefinition = "boolean default false")
+    private Boolean isRemoved = false;
+
 }
