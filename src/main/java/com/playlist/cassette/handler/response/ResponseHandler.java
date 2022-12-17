a7b0b67d9cceda976987426200bbfb9b4bbf6d94
oereo <dlstpgns0406@gmail.com>
2022-12-17 21:33:54 +0900

    refactor: timestamp의 타입을 LocalDateTime으로 수정

--- src/main/java/com/playlist/cassette/handler/response/ResponseHandler.java
+++ src/main/java/com/playlist/cassette/handler/response/ResponseHandler.java
@@ -3,13 +3,17 @@ package com.playlist.cassette.handler.response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ResponseHandler {
     public static ResponseEntity<Object> generateResponse(String message, HttpStatus status, Object data) {
+        LocalDateTime timestamp = LocalDateTime.now();
         Map<String, Object> map = new HashMap<>();
         map.put("message", status.value());
+        map.put("timestamp", timestamp);
         map.put("result", data);
 
         return new ResponseEntity<Object>(map,status);
