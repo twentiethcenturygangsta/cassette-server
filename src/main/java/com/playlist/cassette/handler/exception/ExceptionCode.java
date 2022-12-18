442cddcb34541f187ba6ffcbb0ddc40ba0010886
oereo <dlstpgns0406@gmail.com>
2022-12-18 23:45:14 +0900

    refactor : INVALID_INPUT_NAME_IN_GUEST_FORM 형식으로 수정

--- src/main/java/com/playlist/cassette/handler/exception/ExceptionCode.java
+++ src/main/java/com/playlist/cassette/handler/exception/ExceptionCode.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ExceptionCode {
-    INVALID_INPUT_NAME_IN_GUEST(HttpStatus.BAD_REQUEST, "INVALID_INPUT_NAME_IN_GUEST", "입력한 이름이 올바르지 않습니다.");
+    INVALID_INPUT_NAME_IN_GUEST_FORM(HttpStatus.BAD_REQUEST, "INVALID_INPUT_NAME_IN_GUEST_FORM", "입력한 이름이 올바르지 않습니다.");
 
     private final HttpStatus statusCode;
     private final String code;
