b987a3691db0793dd0c45834f162646e72e8ab4a
oereo <dlstpgns0406@gmail.com>
2022-12-18 23:45:48 +0900

    feat: status code 기준 500 에러가 raise 일경우 에러 핸들러 method 추가

--- src/main/java/com/playlist/cassette/handler/exception/ExceptionControllerAdvice.java
+++ src/main/java/com/playlist/cassette/handler/exception/ExceptionControllerAdvice.java
@@ -1,16 +1,27 @@
 package com.playlist.cassette.handler.exception;
 
 import com.playlist.cassette.handler.response.Error;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class ExceptionControllerAdvice {
 
     @ExceptionHandler
     public ResponseEntity<Error> badRequestExceptionHandle(UserException e) {
-        Error errorResult = new Error(e.getExceptionCode().getCode(), e.getExceptionCode().getMessage());
+        LocalDateTime timestamp = LocalDateTime.now();
+        Error errorResult = new Error(timestamp, e.getExceptionCode().getCode(), e.getExceptionCode().getMessage());
         return new ResponseEntity<>(errorResult, e.getExceptionCode().getStatusCode());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Error> internalServerExceptionHandle(Exception e) {
+        LocalDateTime timestamp = LocalDateTime.now();
+        Error errorResult = new Error(timestamp, "INTERNAL_SERVER_ERROR", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
