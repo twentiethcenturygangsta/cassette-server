4c760e661c6a823566dae44b52d421924754aee3
oereo <dlstpgns0406@gmail.com>
2022-12-18 18:39:04 +0900

    feat: ExceptionCode enum class 추가
    - statusCode
    - code : client가 확인할 코드
    - message : 에러 메시지

---
+++ src/main/java/com/playlist/cassette/handler/exception/ExceptionCode.java
@@ -0,0 +1,22 @@
+package com.playlist.cassette.handler.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ExceptionCode {
+    INVALID_INPUT_NAME_IN_GUEST(HttpStatus.BAD_REQUEST, "INVALID_INPUT_NAME_IN_GUEST", "입력한 이름이 올바르지 않습니다.");
+
+    private final HttpStatus statusCode;
+    private final String code;
+    private final String message;
+
+    //    Bad Request
+
+    ExceptionCode(HttpStatus statusCode, String code, String message) {
+        this.statusCode = statusCode;
+        this.code = code;
+        this.message = message;
+
+    }
+}
