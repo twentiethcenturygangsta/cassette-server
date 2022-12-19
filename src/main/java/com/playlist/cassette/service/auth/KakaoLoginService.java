c908a0e8260ce11316e8defbf32ac7d2eb07aed5
oereo <dlstpgns0406@gmail.com>
2022-12-20 08:28:29 +0900

    feat: kakao login service class private field 추가

--- src/main/java/com/playlist/cassette/service/auth/KakaoLoginService.java
+++ src/main/java/com/playlist/cassette/service/auth/KakaoLoginService.java
@@ -1,2 +1,25 @@
-package com.playlist.cassette.service.auth;public class KakaoLoginService {
+package com.playlist.cassette.service.auth;
+
+import com.playlist.cassette.repository.auth.KakaoLoginRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+
+@RequiredArgsConstructor
+@Service
+public class KakaoLoginService {
+
+    private KakaoLoginRepository kakaoLoginRepository;
+
+    @Value("${kakao.auth-url}")
+    private String kakaoAuthUrl;
+
+    @Value("${kakao.user-api-url}")
+    private String kakaoUserApiUrl;
+
+    @Value("${kakao.rest-api-key}")
+    private String restAPIKey;
+
+    @Value("${kakao.redirect-url}")
+    private String redirectUrl;
 }
