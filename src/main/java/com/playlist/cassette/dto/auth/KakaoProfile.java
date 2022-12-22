c757982bb2a1b80b8a08f7432c7e4fbf277d36ba
oereo <dlstpgns0406@gmail.com>
2022-12-22 19:23:10 +0900

    feat: KakaoProfile dto class 추가
    - kakao API를 통해서 User information 가져올때

---
+++ src/main/java/com/playlist/cassette/dto/auth/KakaoProfile.java
@@ -0,0 +1,11 @@
+package com.playlist.cassette.dto.auth;
+
+import lombok.Getter;
+
+@Getter
+public class KakaoProfile {
+    private String name;
+    private String email;
+    private String gender;
+    private String age;
+}
