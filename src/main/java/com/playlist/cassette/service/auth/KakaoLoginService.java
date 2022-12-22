f98af10d67118ab949853e6bb9211b67be3253ae
oereo <dlstpgns0406@gmail.com>
2022-12-22 19:24:32 +0900

    feat: KakaoLoginService 에 구현 method 추가
    - getProfile
    - getToken

--- src/main/java/com/playlist/cassette/service/auth/KakaoLoginService.java
+++ src/main/java/com/playlist/cassette/service/auth/KakaoLoginService.java
@@ -1,16 +1,21 @@
 package com.playlist.cassette.service.auth;
 
+import com.playlist.cassette.dto.auth.KakaoProfile;
+import com.playlist.cassette.dto.auth.KakaoToken;
 import com.playlist.cassette.repository.auth.KakaoLoginRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Value;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @RequiredArgsConstructor
 @Service
 public class KakaoLoginService {
 
-    private KakaoLoginRepository kakaoLoginRepository;
-
+    private final KakaoLoginRepository kakaoLoginRepository;
+    private static final String AUTHORIZATION_CODE = "authorization_code";
     @Value("${kakao.auth-url}")
     private String kakaoAuthUrl;
 
@@ -22,4 +27,16 @@ public class KakaoLoginService {
 
     @Value("${kakao.redirect-url}")
     private String redirectUrl;
+
+    public KakaoProfile getProfile(String code) throws URISyntaxException {
+        KakaoToken kakaoToken = getToken(code);
+        return kakaoLoginRepository.getProfile(
+                new URI(kakaoUserApiUrl),
+                kakaoToken.getTokenType() + " " + kakaoToken.getAccessToken()
+        );
+    }
+
+    private KakaoToken getToken(String code) throws URISyntaxException {
+        return kakaoLoginRepository.getToken(new URI(kakaoAuthUrl), restAPIKey, redirectUrl, code, AUTHORIZATION_CODE);
+    }
 }
