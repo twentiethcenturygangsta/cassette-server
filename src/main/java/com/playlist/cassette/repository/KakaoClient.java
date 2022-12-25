1a5200e7daafd7841990240872fde94c6d7f54c0
oereo <dlstpgns0406@gmail.com>
2022-12-25 19:28:01 +0900

    refactor : KakaoClient Interface에 Type 수정

--- src/main/java/com/playlist/cassette/repository/KakaoClient.java
+++ src/main/java/com/playlist/cassette/repository/KakaoClient.java
@@ -1,7 +1,7 @@
-package com.playlist.cassette.repository.auth;
+package com.playlist.cassette.repository;
 
 import com.playlist.cassette.config.KakaoFeignConfiguration;
-import com.playlist.cassette.dto.auth.KakaoProfile;
+import com.playlist.cassette.dto.auth.KakaoInfo;
 import com.playlist.cassette.dto.auth.KakaoToken;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,15 +11,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.net.URI;
 
 @FeignClient(name = "kakaoClient", configuration = KakaoFeignConfiguration.class)
-public interface KakaoLoginRepository {
+public interface KakaoClient {
 
     @PostMapping
-    KakaoProfile getProfile(URI baseUrl, @RequestHeader("Authorization") String accessToken);
+    KakaoInfo getProfile(URI baseUrl, @RequestHeader("Authorization") String accessToken);
 
     @PostMapping
     KakaoToken getToken(URI baseUrl, @RequestParam("client_id") String restAPIKey,
                         @RequestParam("redirect_uri") String redirectUrl,
                         @RequestParam("code") String code,
                         @RequestParam("grant_type") String grantType);
-
 }
