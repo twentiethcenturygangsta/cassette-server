7386627cc154c7fc12c1a4cda452ad781cefe873
oereo <dlstpgns0406@gmail.com>
2023-01-01 14:35:04 +0900

    feat: MemberResponseDto class 추가
    - getSocialLoginType method로 소셜로그인 여부 판별

---
+++ src/main/java/com/playlist/cassette/dto/member/MemberResponseDto.java
@@ -0,0 +1,35 @@
+package com.playlist.cassette.dto.member;
+
+import com.playlist.cassette.entity.Member;
+import com.playlist.cassette.entity.SocialLoginType;
+import com.playlist.cassette.handler.exception.ExceptionCode;
+import com.playlist.cassette.handler.exception.UserException;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberResponseDto {
+    private String name;
+    private String email;
+    private SocialLoginType socialLoginType;
+
+    @Builder
+    public MemberResponseDto(Member member) {
+        this.name = member.getName();
+        this.email = member.getEmail();
+        this.socialLoginType = getSocialLoginType(member);
+    }
+
+    private SocialLoginType getSocialLoginType(Member member) {
+        if (!member.getKakaoMemberId().isEmpty()) {
+            return SocialLoginType.KAKAO;
+        }
+        throw new UserException(
+                ExceptionCode.INVALID_SOCIAL_LOGIN_MEMBER,
+                ExceptionCode.INVALID_SOCIAL_LOGIN_MEMBER.getMessage());
+    }
+}
