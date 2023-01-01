5cbe1ddaa70412d3ca52bafa51ccc07f91fce5e0
oereo <dlstpgns0406@gmail.com>
2023-01-01 14:35:28 +0900

    feat: MemberService에 getMember method 추가

--- src/main/java/com/playlist/cassette/service/MemberService.java
+++ src/main/java/com/playlist/cassette/service/MemberService.java
@@ -1,4 +1,23 @@
 package com.playlist.cassette.service;
 
+import com.playlist.cassette.dto.member.MemberResponseDto;
+import com.playlist.cassette.entity.Member;
+import com.playlist.cassette.handler.exception.ExceptionCode;
+import com.playlist.cassette.handler.exception.UserException;
+import com.playlist.cassette.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class MemberService {
+    private final MemberRepository memberRepository;
+    private final KakaoLoginService kakaoLoginService;
+
+    public MemberResponseDto getMember(Long id) {
+        Member member = memberRepository.findById(id).orElseThrow(() ->
+                new UserException(ExceptionCode.INVALID_MEMBER, ExceptionCode.INVALID_MEMBER.getMessage()));
+        MemberResponseDto memberResponseDto = MemberResponseDto.builder().member(member).build();
+        return memberResponseDto;
+    }
 }
