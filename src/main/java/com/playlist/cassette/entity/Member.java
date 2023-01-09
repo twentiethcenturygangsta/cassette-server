e5d96a361bcfb63d46342cd5a464201009863cb8
oereo <dlstpgns0406@gmail.com>
2023-01-09 15:13:16 +0900

    refactor : Member class의 kakaoMemberId field 타입을 String에서 Long으로 수정

--- src/main/java/com/playlist/cassette/entity/Member.java
+++ src/main/java/com/playlist/cassette/entity/Member.java
@@ -23,10 +23,10 @@ public class Member {
     private String email;
     private String gender;
     private String age;
-    private String kakaoMemberId;
+    private Long kakaoMemberId;
 
     @Builder
-    public Member(String name, String email, String gender, String age, String kakaoMemberId) {
+    public Member(String name, String email, String gender, String age, Long kakaoMemberId) {
         this.name = name;
         this.email = email;
         this.gender = gender;
