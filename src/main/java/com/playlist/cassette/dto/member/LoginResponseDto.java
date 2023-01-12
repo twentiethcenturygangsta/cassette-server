1fe2ee1b88e842d8df7c1156ce43b5c8456ea722
oereo <dlstpgns0406@gmail.com>
2023-01-13 02:34:53 +0900

    feat: LoginResponseDto dto class 추가

--- src/main/java/com/playlist/cassette/dto/member/LoginResponseDto.java
+++ src/main/java/com/playlist/cassette/dto/member/LoginResponseDto.java
@@ -1,2 +1,20 @@
-package com.playlist.cassette.dto.member;public class LoginResponseDto {
+package com.playlist.cassette.dto.member;
+
+import com.playlist.cassette.dto.auth.JwtTokenDto;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LoginResponseDto {
+    private MemberResponseDto memberInformation;
+    private JwtTokenDto jwtInformation;
+
+    @Builder
+    public LoginResponseDto(MemberResponseDto memberInformation, JwtTokenDto jwtInformation) {
+        this.memberInformation = memberInformation;
+        this.jwtInformation = jwtInformation;
+    }
 }
