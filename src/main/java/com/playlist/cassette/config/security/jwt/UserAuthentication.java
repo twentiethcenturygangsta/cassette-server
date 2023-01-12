0a6299440dc2da38477ce2ff6951958521e95df0
oereo <dlstpgns0406@gmail.com>
2023-01-13 02:31:58 +0900

    feat: UserAuthentication class 추가

--- src/main/java/com/playlist/cassette/config/security/jwt/UserAuthentication.java
+++ src/main/java/com/playlist/cassette/config/security/jwt/UserAuthentication.java
@@ -1,2 +1,18 @@
-package com.playlist.cassette.config.security.jwt;public class UserAuthentication {
+package com.playlist.cassette.config.security.jwt;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.List;
+
+public class UserAuthentication extends UsernamePasswordAuthenticationToken {
+
+    public UserAuthentication(Long principal, String credentials) {
+        super(principal, credentials);
+    }
+
+    public UserAuthentication(Long principal, String credentials,
+                              List<GrantedAuthority> authorities) {
+        super(principal, credentials, authorities);
+    }
 }
