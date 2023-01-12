b28554d2a7757fc003a011ef6a11444fbbde4abe
oereo <dlstpgns0406@gmail.com>
2023-01-13 02:33:30 +0900

    feat: JwtTokenDto dto class 추가
    - accessToken
    - refreshToken

--- src/main/java/com/playlist/cassette/dto/auth/JwtTokenDto.java
+++ src/main/java/com/playlist/cassette/dto/auth/JwtTokenDto.java
@@ -1,2 +1,19 @@
-package com.playlist.cassette.dto.auth;public class JwtTokenDto {
+package com.playlist.cassette.dto.auth;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class JwtTokenDto {
+    private String accessToken;
+    private String refreshToken;
+
+    @Builder
+    public JwtTokenDto(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
 }
