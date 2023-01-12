3285b15c2ee8d919555c8d30d9e9413a55a07a76
oereo <dlstpgns0406@gmail.com>
2023-01-13 02:32:55 +0900

    feat: Login을 했을 시에 JWT 생성 및 반환 로직 추가와 MemberInformation 추가

--- src/main/java/com/playlist/cassette/service/AuthService.java
+++ src/main/java/com/playlist/cassette/service/AuthService.java
@@ -1,2 +1,35 @@
-package com.playlist.cassette.service;public class AuthService {
+package com.playlist.cassette.service;
+
+import com.playlist.cassette.config.security.jwt.JwtTokenProvider;
+import com.playlist.cassette.config.security.jwt.UserAuthentication;
+import com.playlist.cassette.dto.auth.JwtTokenDto;
+import com.playlist.cassette.dto.member.LoginResponseDto;
+import com.playlist.cassette.dto.member.MemberResponseDto;
+import com.playlist.cassette.entity.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public LoginResponseDto createLoginMember(Member member) {
+        Authentication authentication = new UserAuthentication(member.getId(), null, null);
+        String accessToken = jwtTokenProvider.generateAccessToken(authentication);
+        String refreshToken = jwtTokenProvider.generateRefreshToken(authentication);
+        JwtTokenDto jwtTokenDto = JwtTokenDto.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+        MemberResponseDto memberResponseDto = MemberResponseDto.builder()
+                .member(member)
+                .build();
+        return LoginResponseDto.builder()
+                .jwtInformation(jwtTokenDto)
+                .memberInformation(memberResponseDto)
+                .build();
+    }
 }
