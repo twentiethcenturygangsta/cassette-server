1418e7858d5e23deecd87660c95b36c65a67c745
oereo <dlstpgns0406@gmail.com>
2023-01-14 01:35:17 +0900

    feat: ExceptionCode에 Unauthorized 예외 에러 사항 정의

--- src/main/java/com/playlist/cassette/handler/exception/ExceptionCode.java
+++ src/main/java/com/playlist/cassette/handler/exception/ExceptionCode.java
@@ -13,6 +13,9 @@ public enum ExceptionCode {
 
     // Auth Exception
     EXPIRED_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "EXPIRED_JWT_TOKEN", "만료된 토큰입니다."),
+    INVALID_JWT_SIGNATURE(HttpStatus.UNAUTHORIZED, "INVALID_JWT_SIGNATURE", "토큰 생성키가 올바르지 않습니다."),
+    INVALID_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "INVALID_JWT_TOKEN", "올바르지 않는 토큰입니다."),
+    UNSUPPORTED_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "UNSUPPORTED_JWT_TOKEN", "지원하지 않는 토큰입니다."),
     EMPTY_TOKEN(HttpStatus.UNAUTHORIZED, "EMPTY_TOKEN", "유저 인증이 올바르지 않습니다.");
 
     private final HttpStatus statusCode;
