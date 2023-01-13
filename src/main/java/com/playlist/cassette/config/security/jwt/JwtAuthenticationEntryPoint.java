cf3ac0f819e5b1e30c7024cbff81c2a8e5734518
oereo <dlstpgns0406@gmail.com>
2023-01-14 01:25:37 +0900

    feat: setResponse method 추가
    - exceptionCode에 대해서 response 재정의

--- src/main/java/com/playlist/cassette/config/security/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/playlist/cassette/config/security/jwt/JwtAuthenticationEntryPoint.java
@@ -3,23 +3,36 @@ package com.playlist.cassette.config.security.jwt;
 
 import com.playlist.cassette.handler.exception.ExceptionCode;
 import com.playlist.cassette.handler.exception.UserException;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.json.JSONObject;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
-    private final String AUTHORIZATION_EXCEPTION_ERROR_MESSAGE = "unauthorized access token";
-
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
-            throws IOException, ServletException, UserException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, AUTHORIZATION_EXCEPTION_ERROR_MESSAGE);
+            throws IOException, UserException {
+        ExceptionCode exceptionCode = (ExceptionCode)request.getAttribute("exception");
+        if (exceptionCode.equals(ExceptionCode.EMPTY_TOKEN)) {
+            setResponse(response, ExceptionCode.EMPTY_TOKEN);
+        }
+    }
+
+    private void setResponse(HttpServletResponse response, ExceptionCode exceptionCode) throws IOException {
+        response.setContentType("application/json;charset-UTF-8");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("timestamp", String.valueOf(LocalDateTime.now()));
+        jsonObject.put("code", exceptionCode.getCode());
+        jsonObject.put("message", exceptionCode.getMessage());
+        response.getWriter().print(jsonObject);
     }
 
 }
