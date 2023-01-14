6ae41785eedffd92714e98618e1fcc068e952ebe
jayden.v <jayden.v@kakaocorp.com>
2023-01-15 00:19:36 +0900

    test: 테스트용 커밋

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -11,6 +11,8 @@ TIME_NOW=$(date +%c)
 # 현재 구동 중인 애플리케이션 pid 확인
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 
+echo "jar $JAR_FILE" >> $DEPLOY_LOG
+echo "pid $CURRENT_PID" >> $DEPLOY_LOG
 # 프로세스가 켜져 있으면 종료
 if [ -z $CURRENT_PID ]; then
   echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
