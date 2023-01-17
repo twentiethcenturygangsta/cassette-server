aa79326a06ce6e843fafc0047b31e87f15258038
oereo <dlstpgns0406@gmail.com>
2023-01-18 05:44:30 +0900

    feat: 400 bad request에 대해서 Field Validation 에 대한 에러 처리 구문 추가

--- src/main/java/com/playlist/cassette/handler/exception/ExceptionControllerAdvice.java
+++ src/main/java/com/playlist/cassette/handler/exception/ExceptionControllerAdvice.java
@@ -3,19 +3,26 @@ package com.playlist.cassette.handler.exception;
 import com.playlist.cassette.handler.response.Error;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 @RestControllerAdvice
 public class ExceptionControllerAdvice {
 
-    @ExceptionHandler
-    public ResponseEntity<Error> badRequestExceptionHandle(UserException e) {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Error> badRequestExceptionHandle(MethodArgumentNotValidException e) {
         String timestamp = String.valueOf(LocalDateTime.now());
-        Error errorResult = new Error(timestamp, e.getExceptionCode().getCode(), e.getExceptionCode().getMessage());
-        return new ResponseEntity<>(errorResult, e.getExceptionCode().getStatusCode());
+
+        List<Object> elements = Arrays.asList(e.getBindingResult().getFieldErrors().get(0).getArguments());
+        ;
+        Error errorResult = new Error(
+                timestamp, ((ExceptionCode) elements.get(1)).getCode(), ((ExceptionCode) elements.get(1)).getMessage());
+        return new ResponseEntity<>(errorResult, e.getStatusCode());
     }
 
     @ExceptionHandler
