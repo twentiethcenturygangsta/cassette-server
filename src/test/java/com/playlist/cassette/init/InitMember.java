82f01f75e4853c3e7892134fef0ff0fa5dc31ecf
oereo <dlstpgns0406@gmail.com>
2023-01-24 23:59:35 +0900

    feat: test case 작성 시 기본 instance 생성 class 추가

--- src/test/java/com/playlist/cassette/init/InitMember.java
+++ src/test/java/com/playlist/cassette/init/InitMember.java
@@ -1,2 +1,17 @@
-package com.playlist.cassette.init;public class InitMember {
+package com.playlist.cassette.init;
+
+import com.playlist.cassette.entity.Member;
+
+import java.util.Date;
+
+public class InitMember {
+    public Member createDefaultMember() {
+        return Member.builder()
+                .name("김중앙")
+                .email("test1@cassette.com")
+                .gender("male")
+                .age("20-29")
+                .kakaoMemberId(1L)
+                .build();
+    }
 }
