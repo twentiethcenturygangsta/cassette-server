b5d4106df488c857928d06ef163fd1242e0d3edd
oereo <dlstpgns0406@gmail.com>
2023-01-25 01:43:23 +0900

    feat: member 객체 생성 test case와 updateRefreshToken Test case 추가

--- src/test/java/com/playlist/cassette/entity/MemberTest.java
+++ src/test/java/com/playlist/cassette/entity/MemberTest.java
@@ -1,11 +1,14 @@
 package com.playlist.cassette.entity;
 
+import com.playlist.cassette.dto.auth.TokenDto;
 import com.playlist.cassette.init.InitMember;
 import com.playlist.cassette.repository.MemberRepository;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Date;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -28,6 +31,24 @@ public class MemberTest {
     @AfterEach
     void afterSetup() {
         memberRepository.delete(member);
+    }
+
+    @Test
+    @DisplayName("멤버 객체 생성 성공")
+    @Transactional
+    public void test_create_member_성공() {
+        Optional<Member> savedMember = memberRepository.findByKakaoMemberId(1L);
+        savedMember.ifPresent(presentedMember -> {
+            assertThat(presentedMember.getId()).isEqualTo(1);
+            assertThat(presentedMember.getName()).isEqualTo("김중앙");
+            assertThat(presentedMember.getEmail()).isEqualTo("test1@cassette.com");
+            assertThat(presentedMember.getRefreshToken()).isNull();
+            assertThat(presentedMember.getRefreshTokenExpireTime()).isNull();
+            assertThat(presentedMember.getAge()).isEqualTo("20-29");
+            assertThat(presentedMember.getGender()).isEqualTo("male");
+            assertThat(presentedMember.getTapes()).isEqualTo(member.getTapes());
+            assertThat(presentedMember.getKakaoMemberId()).isEqualTo(1L);
+        });
     }
 
     @Nested
@@ -58,6 +79,28 @@ public class MemberTest {
             Optional<Member> savedMember = memberRepository.findByKakaoMemberId(1L);
             savedMember.ifPresent(presentedMember -> {
                 assertThat(presentedMember.getIsRemoved()).isEqualTo(false);
+            });
+        }
+    }
+
+    @Nested
+    @DisplayName("member 객체의 refreshToken, expiredTime update 테스트")
+    class TestMemberUpdateRefreshToken {
+        @Test
+        @DisplayName("refreshToken, expiredTime이 수정이 된다.")
+        public void test_update_refresh_token_in_member_성공() {
+            Date expiredTime = new Date();
+
+            Optional<Member> savedMember = memberRepository.findByKakaoMemberId(1L);
+            TokenDto tokenDto = TokenDto.builder()
+                    .value("abcd")
+                    .expiredTime(expiredTime)
+                    .build();
+
+            savedMember.ifPresent(presentedMember -> {
+                presentedMember.updateRefreshToken(tokenDto);
+                assertThat(presentedMember.getRefreshToken()).isEqualTo(tokenDto.getValue());
+                assertThat(presentedMember.getRefreshTokenExpireTime()).isEqualTo(tokenDto.getExpiredTime());
             });
         }
     }
