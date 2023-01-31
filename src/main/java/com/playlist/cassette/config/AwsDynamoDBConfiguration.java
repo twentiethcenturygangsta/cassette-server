11a49e5f308b73a9997645ede40ba794f7efaf00
oereo <dlstpgns0406@gmail.com>
2023-01-31 18:59:34 +0900

    chore: AwsDynamoDBConfiguration 추가

---
+++ src/main/java/com/playlist/cassette/config/AwsDynamoDBConfiguration.java
@@ -0,0 +1,11 @@
+package com.playlist.cassette.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+
+public class AwsDynamoDBConfiguration {
+}
