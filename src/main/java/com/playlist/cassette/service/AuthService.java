a0184704821d154c6cd23dc09d8bc13a4c6e4b63
oereo <dlstpgns0406@gmail.com>
2023-02-09 17:54:54 +0900

    feat: Access-Control_Expose-Headers property 추가

--- src/main/java/com/playlist/cassette/service/AuthService.java
+++ src/main/java/com/playlist/cassette/service/AuthService.java
@@ -104,7 +104,7 @@ public class AuthService {
         cookie.setPath("/");
 
         response.addCookie(cookie);
-        response.addHeader("Access-Control-Expose-Headers", "Set-Cookie");
+        response.addHeader("Access-Control-Expose-Headers", "set-cookie");
     }
 }
 
