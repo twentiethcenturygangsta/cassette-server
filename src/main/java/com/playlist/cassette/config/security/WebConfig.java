8ddd2962e391c31f7287b8b2665fae2cb1039541
oereo <dlstpgns0406@gmail.com>
2023-02-11 07:03:04 +0900

    feat: test용 WebClient host 추가

--- src/main/java/com/playlist/cassette/config/security/WebConfig.java
+++ src/main/java/com/playlist/cassette/config/security/WebConfig.java
@@ -12,7 +12,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://www.12playlist.com", "https://www.gangstatest.12playlist.com")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://www.12playlist.com",
+                        "https://www.gangstatest.12playlist.com",
+                        "https://cassette-git-develop-kimkyungmin123.vercel.app"
+                )
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .exposedHeaders("refreshToken")
