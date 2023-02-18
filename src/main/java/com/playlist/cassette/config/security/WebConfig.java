2a366a99bb137bc94b681ac051ccfc784a85670d
oereo <dlstpgns0406@gmail.com>
2023-02-18 12:32:07 +0900

    feat: add allowed origin rollback

--- src/main/java/com/playlist/cassette/config/security/WebConfig.java
+++ src/main/java/com/playlist/cassette/config/security/WebConfig.java
@@ -12,21 +12,20 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-//                .allowedOrigins(
-//                        "http://localhost:3000",
-//                        "https://www.12playlist.com",
-//                        "https://ganstatest.12playlist.com",
-//                        "https://www.ganstatest.12playlist.com",
-//                        "https://12playlist.com",
-//                        "https://www.gangstatest.12playlist.com",
-//                        "https://gangstatest.12playlist.com",
-//                        "https://api.12playlist.com",
-//                        "https://cassette-git-develop-kimkyungmin123.vercel.app"
-//                )
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://www.12playlist.com",
+                        "https://ganstatest.12playlist.com",
+                        "https://www.ganstatest.12playlist.com",
+                        "https://12playlist.com",
+                        "https://www.gangstatest.12playlist.com",
+                        "https://gangstatest.12playlist.com",
+                        "https://api.12playlist.com",
+                        "https://cassette-git-develop-kimkyungmin123.vercel.app"
+                )
                 .allowedMethods("*")
                 .allowedHeaders("*")
-                .exposedHeaders("refreshToken");
-//                .allowCredentials(true);
+                .exposedHeaders("refreshToken")
+                .allowCredentials(true);
     }
 }
