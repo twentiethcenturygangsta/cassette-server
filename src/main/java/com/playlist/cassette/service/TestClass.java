28911f6432013a2762fec6c06c59d4cb68963a18
oereo <dlstpgns0406@gmail.com>
2023-03-05 18:31:22 +0900

    refactor: module import config

--- src/main/java/com/playlist/cassette/service/TestClass.java
+++ src/main/java/com/playlist/cassette/service/TestClass.java
@@ -1,18 +1,13 @@
 package com.playlist.cassette.service;
 
-import com.twentiethcenturygangsta.ourboard.auth.Role;
 import com.twentiethcenturygangsta.ourboard.auth.UserCredentials;
-import com.twentiethcenturygangsta.ourboard.database.UserDatabaseCredentials;
+import com.twentiethcenturygangsta.ourboard.auth.UserDatabaseCredentials;
 import com.twentiethcenturygangsta.ourboard.site.OurBoardClient;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.sql.SQLException;
 
 
 @Slf4j
